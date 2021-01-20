90f6c966d879118ce0af85d1272cc4c2d62ffff1
kaiyuan zhao <zhao.kaiy@husky.neu.edu>
2021-01-20 14:12:33 -0500

    commit for autolayout

---
+++ Calculator-Layout-iOS13/Calculator Layout iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator Layout iOS13
+//
+//  Created by Angela Yu on 01/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var user: Bool?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
